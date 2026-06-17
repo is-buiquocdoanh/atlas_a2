5b52ddde371b46288041120637c6a0f829891b51
Jayson_Bui <doanh762003@gmail.com>
2026-06-17 23:59:16 +0700

    add src

--- setup/install.sh
+++ setup/install.sh
@@ -43,7 +43,7 @@ User=$USER_NAME
 Group=$USER_NAME
 Environment="HOME=$USER_HOME"
 Environment="ROS_DOMAIN_ID=04"
-ExecStartPre=/bin/sleep 30
+ExecStartPre=/bin/sleep 15
 ExecStart=$WORKSPACE/setup/start_bringup.sh
 Restart=on-failure
 RestartSec=5
